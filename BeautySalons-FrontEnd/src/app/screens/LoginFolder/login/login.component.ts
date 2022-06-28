88ffba4c63134453193d561a4b08b18604ecaf7e
Nir Razo <razonir@gmail.com>
2022-06-28 18:51:20 +0300

    login,logout,signin

--- BeautySalons-FrontEnd/src/app/screens/LoginFolder/login/login.component.ts
+++ BeautySalons-FrontEnd/src/app/screens/LoginFolder/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/model/user';
 import { UserService } from 'src/app/services/user.service';
+import { Router, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -17,49 +18,67 @@ export class LoginComponent implements OnInit {
   user: User = new User();
   userlogin: User = new User();
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
     console.log(this.userlogin.useremail)
   }
-
-
-  show(){
+  show() {
     this.displayPopup = 'flex';
   }
-  hidePopup(){
+  hidePopup() {
     this.displayPopup = 'none';
   }
-  onClickmoveup(){
-    if(this.user.useremail==undefined || this.user.useremail=='' ||
-    this.user.userfname==undefined || this.user.userfname=='' ||
-    this.user.userlname==undefined || this.user.userlname=='' ||
-    this.user.userpassword==undefined || this.user.userpassword=='' ||
-    this.user.userphone==undefined || this.user.userphone==''){
+  onClickmoveup() {
+    if (this.user.useremail == undefined || this.user.useremail == '' ||
+      this.user.userfname == undefined || this.user.userfname == '' ||
+      this.user.userlname == undefined || this.user.userlname == '' ||
+      this.user.userpassword == undefined || this.user.userpassword == '' ||
+      this.user.userphone == undefined || this.user.userphone == '') {
       this.displysignuperrors = 'flex';
-    }else{
+    } else {
       this.moveup = '-600px';
       this.displayStep = 'flex';
       this.displysignuperrors = 'none';
 
     }
   }
-  backStep(){
+  backStep() {
     this.displayStep = 'none';
     this.moveup = '0px'
   }
-  signup(){
-    if(this.user.usergender==undefined || this.user.usergender=='' ||
-    this.user.usercity==undefined || this.user.usercity=='' ||
-    this.user.useraddress==undefined || this.user.useraddress==''){
+  signup() {
+    if (this.user.usergender == undefined || this.user.usergender == '' ||
+      this.user.usercity == undefined || this.user.usercity == '' ||
+      this.user.useraddress == undefined || this.user.useraddress == '') {
       this.displysignuperrors = 'flex';
-    }else{
+    } else {
       console.log(this.user);
+      this.userService.createUser(this.user).subscribe(
+        response => this.goToHome(),
+        error => console.error('Error!', error)
+      );
     }
   }
 
-  login(){
-    this.displyloginerrors = 'flex';
-    console.log(this.userlogin);
+  login() {
+
+    if (this.userlogin.useremail == undefined || this.userlogin.useremail == '' ||
+      this.userlogin.userpassword == undefined || this.userlogin.userpassword == '') {
+      this.displyloginerrors = 'flex';
+    } else {
+      console.log(this.userlogin);
+      this.userService.login(this.userlogin).subscribe(
+        response => {
+          localStorage.setItem("uid", response.uid),
+            localStorage.setItem("token", response.token),
+            this.goToHome()
+        },
+        error => console.error('Error!', error)
+      );
+    }
+  }
+  goToHome() {
+    this.router.navigate(['']);
   }
 }
--- BeautySalons-FrontEnd/src/app/services/user.service.ts
+++ BeautySalons-FrontEnd/src/app/services/user.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
-import { User } from '../model/user'
+import { HttpClient , HttpHeaders, HttpParams  } from '@angular/common/http';
+import { User } from '../model/user';
+import { Observable, BehaviorSubject } from "rxjs";
+import { first, catchError, tap } from "rxjs/operators";
+import { ErrorHandlerService } from "./error-handler.service";
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private baseURL = "http://localhost:3001/user/";
+  constructor(private httpClient: HttpClient) { }
+
+  httpOptions: { headers: HttpHeaders } = {
+    headers: new HttpHeaders({ "Content-Type": "application/json" }),
+  };
+
+  createUser(user: User){
+    let registerUrl = this.baseURL+'createUser'
+    return this.httpClient.post<any>(`${registerUrl}`,user);
+  }
+  login(user: User){
+    let login = this.baseURL+'login'
+    return this.httpClient.post<any>(`${login}`,user);
+  }
+  
+  allUsers(){
+    let users = this.baseURL+'users'
+    return this.httpClient.get(users);
+  }
+
 }
