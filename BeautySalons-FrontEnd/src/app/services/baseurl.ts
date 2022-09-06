6d4f26bed4a2b559397da9f06883d7c3a01fc479
Nir Razo <razonir@gmail.com>
2022-09-07 00:20:30 +0300

    url

--- BeautySalons-FrontEnd/src/app/services/baseurl.ts
+++ BeautySalons-FrontEnd/src/app/services/baseurl.ts
@@ -1,7 +1,7 @@
-export const serverUrl = {
-    url: "http://localhost:3001/"
-}
-
 // export const serverUrl = {
-//    url: "https://beautysalons-backend.herokuapp.com/"
+//     url: "http://localhost:3001/"
 // }
+
+export const serverUrl = {
+   url: "https://beautysalons-backend.herokuapp.com/"
+}
