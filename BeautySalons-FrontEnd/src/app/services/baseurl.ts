6659059357c59589d5e5f6501180dda3feb81eaf
Razo <razonir@gmail.com>
2022-09-10 11:11:13 +0300

    aws

--- BeautySalons-FrontEnd/src/app/services/baseurl.ts
+++ BeautySalons-FrontEnd/src/app/services/baseurl.ts
@@ -1,7 +1,14 @@
+// localhost
 // export const serverUrl = {
 //     url: "http://localhost:3001/"
 // }
 
+// aws
 export const serverUrl = {
-   url: "https://beautysalons-backend.herokuapp.com/"
+   url: "http://beautys-env.eba-wpj2qbx3.eu-west-1.elasticbeanstalk.com/"
 }
+
+// heroku
+// export const serverUrl = {
+//    url: "https://beautysalons-backend.herokuapp.com/"
+// }
