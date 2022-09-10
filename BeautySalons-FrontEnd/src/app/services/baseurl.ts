a84498ca6656582985b327b6b9c385a8179dca80
Razo <razonir@gmail.com>
2022-09-10 11:35:41 +0300

    render

--- BeautySalons-FrontEnd/src/app/services/baseurl.ts
+++ BeautySalons-FrontEnd/src/app/services/baseurl.ts
@@ -4,11 +4,16 @@
 // }
 
 // aws
-export const serverUrl = {
-   url: "http://beautys-env.eba-wpj2qbx3.eu-west-1.elasticbeanstalk.com/"
-}
+// export const serverUrl = {
+//    url: "http://beautys-env.eba-wpj2qbx3.eu-west-1.elasticbeanstalk.com/"
+// }
 
 // heroku
 // export const serverUrl = {
 //    url: "https://beautysalons-backend.herokuapp.com/"
 // }
+
+// render
+export const serverUrl = {
+   url: "https://beautysalons-backend.onrender.com/"
+}
