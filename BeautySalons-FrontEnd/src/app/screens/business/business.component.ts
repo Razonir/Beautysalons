ac4b2bad4eb2b9f5092b72bde4db555b4829c657
Nir Razo <razonir@gmail.com>
2022-10-18 01:07:01 +0300

    colors

--- BeautySalons-FrontEnd/src/app/screens/business/business.component.ts
+++ BeautySalons-FrontEnd/src/app/screens/business/business.component.ts
@@ -6,18 +6,17 @@ import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-business',
   templateUrl: './business.component.html',
-  styleUrls: ['./business.component.scss']
+  styleUrls: ['./business.component.scss'],
 })
 export class BusinessComponent implements OnInit {
-
-  gender:any;
-  background = 'linear-gradient(90deg, #6a18c7, #9d69da)'
-  color = '#6a18c7'
-
+  gender: any;
+  background = 'linear-gradient(90deg, #6a18c7, #9d69da)';
+  color = '#6a18c7';
 
   businessData: any;
   userT: any;
-  category:any;
+
+  category: any;
   load = true;
   businessDataHair: any;
   businessDataEyebrows: any;
@@ -25,30 +24,37 @@ export class BusinessComponent implements OnInit {
   businessDataMakeup: any;
   businessDataCosmatics: any;
   businessDataNail: any;
+  lenbusinessData: any;
   lenbusinessDataHair: any;
   lenbusinessDataEyebrows: any;
   lenbusinessDataTattoo: any;
   lenbusinessDataMakeup: any;
   lenbusinessDataCosmatics: any;
   lenbusinessDataNail: any;
-  displaybusinessDataHair='none';
-  displaybusinessDataEyebrows='none';
-  displaybusinessDataTattoo='none';
-  displaybusinessDataMakeup='none';
-  displaybusinessDataCosmatics='none';
-  displaybusinessDataNail='none';
+  displaybusinessDataHair = 'none';
+  displaybusinessDataEyebrows = 'none';
+  displaybusinessDataTattoo = 'none';
+  displaybusinessDataMakeup = 'none';
+  displaybusinessDataCosmatics = 'none';
+  displaybusinessDataNail = 'none';
   categoryshow = 'none';
   categoryhide = 'none';
-  constructor(private router: Router, private businessService: BusinessService, private route: ActivatedRoute) { }
+  constructor(
+    private router: Router,
+    private businessService: BusinessService,
+    private route: ActivatedRoute
+  ) {}
   ngOnInit(): void {
-    this.category = this.route.snapshot.queryParamMap.get('category')
-    if(this.category == undefined ){
+    this.category = this.route.snapshot.queryParamMap.get('category');
+    if (this.category == undefined) {
       this.categoryhide = 'flex';
 
-      this.businessService.getAll().subscribe((data)=>{
+      this.businessService.getAll().subscribe((data) => {
         this.load = false;
         this.businessData = data;
-        this.businessData = this.businessData.filter((d: { bgender: any; }) => d.bgender == this.gender);
+        this.businessData = this.businessData.filter(
+          (d: { bgender: any }) => d.bgender == this.gender
+        );
         this.businessDataHair = this.businessData.filter(
           (b: { bsubject: any }) => b.bsubject == 'מספרות'
         );
@@ -67,74 +73,83 @@ export class BusinessComponent implements OnInit {
         this.businessDataNail = this.businessData.filter(
           (b: { bsubject: any }) => b.bsubject == 'ציפורניים'
         );
+        this.lenbusinessData = this.businessData.length;
         this.lenbusinessDataHair = this.businessDataHair.length;
         this.lenbusinessDataEyebrows = this.businessDataEyebrows.length;
         this.lenbusinessDataTattoo = this.businessDataTattoo.length;
         this.lenbusinessDataMakeup = this.businessDataMakeup.length;
         this.lenbusinessDataCosmatics = this.businessDataCosmatics.length;
         this.lenbusinessDataNail = this.businessDataNail.length;
-        if(this.lenbusinessDataHair>0){
-          this.displaybusinessDataHair='block';
+        if (this.lenbusinessDataHair > 0) {
+          this.displaybusinessDataHair = 'block';
         }
-        if(this.lenbusinessDataEyebrows>0){
-          this.displaybusinessDataEyebrows='block';
+        if (this.lenbusinessDataEyebrows > 0) {
+          this.displaybusinessDataEyebrows = 'block';
         }
-        if(this.lenbusinessDataTattoo>0){
-          this.displaybusinessDataTattoo='block';
+        if (this.lenbusinessDataTattoo > 0) {
+          this.displaybusinessDataTattoo = 'block';
         }
-        if(this.lenbusinessDataMakeup>0){
-          this.displaybusinessDataMakeup='block';
+        if (this.lenbusinessDataMakeup > 0) {
+          this.displaybusinessDataMakeup = 'block';
         }
-        if(this.lenbusinessDataCosmatics>0){
-          this.displaybusinessDataCosmatics='block';
+        if (this.lenbusinessDataCosmatics > 0) {
+          this.displaybusinessDataCosmatics = 'block';
         }
-        if(this.lenbusinessDataNail>0){
-          this.displaybusinessDataNail='block';
+        if (this.lenbusinessDataNail > 0) {
+          this.displaybusinessDataNail = 'block';
         }
-        
       });
-    }else{
-      this.businessService.getAll().subscribe((data)=>{
+    } else {
+      this.businessService.getAll().subscribe((data) => {
         this.categoryshow = 'flex';
         this.categoryhide = 'none';
         this.load = false;
         this.businessData = data;
-        this.businessData = this.businessData.filter((d: { bgender: any; }) => d.bgender == this.gender);
-        this.businessData = this.businessData.filter((s: { bsubject: any; }) => s.bsubject == this.category);
+        this.businessData = this.businessData.filter(
+          (s: { bsubject: any }) => s.bsubject == this.category
+        );
+        if(this.businessData.length == 0){
+          this.businessData = data;
+        }
+        this.businessData = this.businessData.filter(
+          (d: { bgender: any }) => d.bgender == this.gender
+        );
       });
     }
     this.gender = localStorage.getItem('gender');
-    if(this.gender == null || this.gender == 'female'){
+    if (this.gender == null || this.gender == 'female') {
       this.gender = 'female';
-      localStorage.setItem('gender','female')
+      localStorage.setItem('gender', 'female');
     }
-
   }
 
-  city(icity:string,baddress:string,bname:string){
-    if(this.userT == undefined){
+  city(icity: string, baddress: string, bname: string) {
+    if (this.userT == undefined) {
       return true;
     }
-    icity = icity.toLocaleLowerCase()
-    baddress = baddress.toLocaleLowerCase()
-    bname = bname.toLocaleLowerCase()
-    this.userT = this.userT.toLocaleLowerCase()
-    baddress = baddress.replace(" ", "")
-    icity = icity.replace(" ", "")
-    bname = bname.replace(" ", "")
-    let len = this.userT.length; 
-
-    if(icity.substring(0,len)==this.userT.replace(" ", "") || bname.substring(0,len)==this.userT.replace(" ", "")  || baddress.substring(0,len)==this.userT.replace(" ", "")){
-        return true;
+    let len = this.userT.length;
+    let src = icity + baddress + bname;
+    let res = this.userT.replace(/\s/g, '');
+    src = src.toLowerCase();
+    res = res.toLowerCase();
+    src = src.replace(/\s/g, '');
+    if (src.includes(res)) {
+      return true;
     }
     return false;
   }
 
-  step(){
+  step() {
     this.categoryshow = 'flex';
     this.categoryhide = 'none';
-    this.category = this.route.snapshot.queryParamMap.get('category')
-    this.businessData = this.businessData.filter((s: { bsubject: any; }) => s.bsubject == this.category);
-
+    this.category = this.route.snapshot.queryParamMap.get('category');
+    this.businessData = this.businessData.filter(
+      (s: { bsubject: any }) => s.bsubject == this.category
+    );
   }
+  all() {
+    this.categoryshow = 'flex';
+    this.categoryhide = 'none';
+  }
+
 }
