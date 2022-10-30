0e04a3fd433e9724b85102ee8a9b1e821cf7385a
Nir Razo <razonir@gmail.com>
2022-10-31 00:10:30 +0200

    title

--- BeautySalons-FrontEnd/src/app/screens/error/error.component.ts
+++ BeautySalons-FrontEnd/src/app/screens/error/error.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Title} from '@angular/platform-browser'
 
 @Component({
   selector: 'app-error',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ErrorComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private titleService: Title
+  ) { 
+  }
 
   ngOnInit(): void {
+    this.titleService.setTitle("דף ללא מוצא")
   }
 
 }
